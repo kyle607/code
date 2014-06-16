2133f1c487bbb2ca50cabfa4ad925386fe69da84
Kyle Kirk <kyle607@gmail.com>
2014-06-16 13:05:05 -0500

    Implemented initialize method to Cookbook class  that adds a title to cookbook.  Implemented initialize method to Recipe class that adds title, ingredients, and steps to Recipe.

--- cookbook.rb
+++ cookbook.rb
@@ -1,8 +1,13 @@
 class Cookbook
-  def cookbook(title)
+  def initialize(title)
     @title = title
   end
 end
 
 class Recipe
+  def initialize(title, ingredients, steps)
+    @title = title
+    @ingredients = ingredients
+    @steps = steps
+  end
 end
